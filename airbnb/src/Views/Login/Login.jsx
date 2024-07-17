c6361ccb20006f1a9b00cc7bf0d9a02ab0850f93
MariusNaydenov <marius.naydenov1994@gmail.com>
2024-07-17 15:00:34 +0300

    change the local url path

--- airbnb/src/Views/Login/Login.jsx
+++ airbnb/src/Views/Login/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/api/login`,
+        `${import.meta.env.VITE_API_URL}/login`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
